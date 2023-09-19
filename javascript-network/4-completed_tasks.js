164930001aae7150ed0a86746cdec2e167f86d1b
TshegoR24 <cavinrakoma@gmail.com>
2023-09-19 11:00:07 +0200

    java network

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,27 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-// Get the API URL from the first argument
-const apiUrl = process.argv[2];
-// Make a request to the API
-request(apiUrl, function (error, response, body) {
-  // Check for errors
+const url = process.argv[2];
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-    return;
+    console.error('Error:', error);
   }
-  // Parse the response body as JSON
-  const todos = JSON.parse(body);
-  // Get the user IDs of the completed tasks
-  const completedTaskUserIds = todos
-    .filter(todo => todo.completed)
-    .map(todo => todo.userId);
-  // Count the number of tasks completed by each user
-  const taskCountsByUserId = completedTaskUserIds.reduce((counts, userId) => {
-    counts[userId] = (counts[userId] || 0) + 1;
-    return counts;
-  }, {});
-  // Print the users with completed tasks
-  for (const userId in taskCountsByUserId) {
-    console.log(`${userId}: ${taskCountsByUserId[userId]}`);
-  }
+  const tasks = JSON.parse(body);
+  const completed = tasks.filter((task) => task.completed);
+  let counts = {};
+  completed.forEach((task) => {
+    const id = task.userId;
+    if (!counts[id]) {
+      counts[id] = 1;
+    } else {
+      counts[id]++;
+    }
+  });
+  console.log(counts);
 });
