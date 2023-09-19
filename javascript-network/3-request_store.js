8e7b2e770643fe1e61de53935aee04bfd2a1d18b
TshegoR24 <cavinrakoma@gmail.com>
2023-09-19 10:57:43 +0200

    java network

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,25 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-// Get the URL from the first argument
 const url = process.argv[2];
-// Get the file path from the second argument
-const filePath = process.argv[3];
-// Make a request to the URL
-request(url, function (error, response, body) {
-  // Check for errors
+const file = process.argv[3];
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-    return;
+    console.error('Error:', error);
   }
-  // Save the response body to a file
-  fs.writeFile(filePath, body, 'utf-8', function (error) {
-    // Check for errors
+  fs.writeFile(file, body, 'utf-8', (error) => {
     if (error) {
-      console.error(error);
-      return;
+      console.error('Error:', error);
     }
-    // Log a success message
-    console.log('The file was saved successfully.');
   });
 });
