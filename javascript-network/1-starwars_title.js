30bd12055910f672ff03ee23d11bbdb9ca3e3e1f
TshegoR24 <cavinrakoma@gmail.com>
2023-09-19 08:27:52 +0200

    java

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,28 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const args = process.argv.slice(2);
-
-if (args.length !== 1) {
-    console.error('Usage: node getStarWarsMovieTitle.js <episode_id>');
-    process.exit(1);
-}
-
-const episodeId = args[0];
-
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${episodeId}`;
-
-request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error(`Error: ${error}`);
-        process.exit(1);
-    }
-
-    if (response.statusCode === 200) {
-        const movieData = JSON.parse(body);
-        const movieTitle = movieData.title;
-        console.log(`The title of Episode ${episodeId} is: ${movieTitle}`);
-    } else {
-        console.error(`Error: Unable to retrieve movie information for Episode ${episodeId}`);
-        process.exit(1);
-    }
+// Get the movie ID from the first argument
+const movieId = parseInt(process.argv[2]);
+// Make a request to the Star Wars API
+request(`https://swapi-api.alx-tools.com/api/films/${movieId}`, function (error, response, body) {
+  // Check for errors
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Parse the response body as JSON
+  const movie = JSON.parse(body);
+  // Print the movie title
+  console.log(movie.title);
 });
